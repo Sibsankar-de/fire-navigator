d270c8d0ce9cb322fdd7dc9917590872ed4343d2
Sibsankar-de <sibsankarde910@gmail.com>
2025-11-22 02:49:30 +0530

    feat: Home screen added

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,98 +1,127 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Entypo, FontAwesome5, MaterialCommunityIcons, MaterialIcons, Octicons } from '@expo/vector-icons';
+import { Stack } from 'expo-router';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: "#f0bd66ff",
+          },
+          headerTitle: () => (
+            <View style={{ flexDirection: "column" }}>
+              <Text style={{ fontSize: 20, fontWeight: "600" }}>Fire Navigator</Text>
+            </View>
+          ),
+          headerLeft: () => (
+            <FontAwesome5 name="fire" size={30} style={{ marginRight: 10, color: "#c46a0aff" }} />
+          ),
+        }}
+      />
+      <View>
+        <View style={{ padding: 13}}>
+          <View style={{ marginBottom: 13 }}>
+            <View style={{ flexDirection: "row", gap: 5, alignItems: "center" }}>
+              <FontAwesome5 name="building" size={15} />
+              <Text style={{ fontSize: 20 }}>Buildings</Text>
+            </View>
+            <Text style={[{ fontSize: 14 }, styles.textLight]}>Select or search a building to navigate.</Text>
+          </View>
+          <TextInput
+            placeholder="Search buildings"
+            // value={text}
+            // onChangeText={setText}
+            style={styles.searchInput}
+          />
+        </View>
+        <ScrollView style={{ padding: 13 }}>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+          <View>
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+            <BuildingCard />
+          </View>
+        </ScrollView>
+      </View>
+    </>
   );
 }
 
+function BuildingCard() {
+  return (
+    <View style={styles.cardBox}>
+      <View style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
+        <View style={styles.buildingIconBox}>
+          <MaterialCommunityIcons name="office-building" size={40} style={styles.buildingIcon} />
+        </View>
+        <View>
+          <Text style={{ fontSize: 20 }}>Lorem, ipsum.</Text>
+          <View style={{ flexDirection: "row", gap: 2, alignItems: "center", marginBottom: 10 }}>
+            <Entypo name="location-pin" size={15} style={styles.textLight} />
+            <Text style={styles.textLight}>Lorem ipsum dolor sit amet consectetu</Text>
+          </View>
+          <View style={{ flexDirection: "row", gap: 15, alignItems: "center" }}>
+            <View style={{ flexDirection: "row", gap: 2, alignItems: "center" }}>
+              <Octicons name="stack" size={15} style={styles.textLight} />
+              <Text style={styles.textLight}>8 floors</Text>
+            </View>
+            <View style={{ flexDirection: "row", gap: 2, alignItems: "center" }}>
+              <MaterialIcons name="sensors" size={15} style={styles.textLight} />
+              <Text style={styles.textLight}>40 sensors</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  buildingIconBox: {
+    padding: 8,
+    backgroundColor: 'rgba(172, 215, 229, 0.28)',
+    borderRadius: 10
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  buildingIcon: {
+    color: 'rgba(95, 139, 140, 0.53)'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  cardBox: {
+    marginVertical: 8,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+    borderLeftWidth: 3,
+    borderLeftColor: "rgba(96, 212, 94, 1)"
   },
+  textLight: {
+    color: "rgba(87, 87, 87, 1)"
+  },
+  searchInput: {
+    height: 50,
+    backgroundColor: "#ccebf084",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    borderColor: "#b3b3b3ff",
+  }
 });
